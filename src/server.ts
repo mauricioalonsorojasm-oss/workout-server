1e4d631de3f5d49c6ef12eea371154ccc21a27e7
Maria Pol <maria.pujolpol@gmail.com>
2026-03-20 11:46:24 +0100

    fix

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import app from "./app";
 
-const PORT = process.env.PORT; // 👈 SIN 5005
+const PORT = process.env.PORT; 
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
